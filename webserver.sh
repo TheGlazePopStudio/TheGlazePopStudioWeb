a074c4f9abd74ac633057276a84c1f7592da0e21
ranger097 <ranger097@proton.me>
2026-05-09 21:53:25 -0400

    i added scrolling banner, sample images and product description, cool and simple button animations, a full screen video on the landing page to show concepts, i deleted all inspiration pages and example images now that the website has a clear design language.

--- webserver.sh
+++ webserver.sh
@@ -1,4 +1,6 @@
-pkill live-server
+#/usr/bin/env
+pkill live-server &
 live-server --port=8080 >/dev/null 2>&1 &
-xdg-open http://localhost:8080 &
+xdg-open http://localhost:8080 >/dev/null 2>&1 &
+nvim .
 
